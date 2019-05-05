51654be8688b66d7248db77c5041dcecd9051372
diguaganer <niushuai951104@gmail.com>
2019-05-05 22:47:15 +0800

    测试类 初始化

--- src/main/java/cc/niushuai/graduate/test/controller/TestController.java
+++ src/main/java/cc/niushuai/graduate/test/controller/TestController.java
@@ -3,9 +3,7 @@ package cc.niushuai.graduate.test.controller;
 import cc.niushuai.graduate.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author niush
@@ -17,11 +15,5 @@ public class TestController {
     @Autowired
     private MailService mailService;
 
-
-    @GetMapping("/mail")
-    @ResponseBody
-    public void testSend(){
-        mailService.testSend("1225803134@qq.com");
-    }
 
 }
