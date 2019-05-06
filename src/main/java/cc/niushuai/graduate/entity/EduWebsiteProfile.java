9679866282eec8a764f54347afe3afd909816090
diguaganer <niushuai951101@gmail.com>
2019-05-06 12:54:29 +0800

    date加入jsonFormat

--- src/main/java/cc/niushuai/graduate/entity/EduWebsiteProfile.java
+++ src/main/java/cc/niushuai/graduate/entity/EduWebsiteProfile.java
@@ -8,10 +8,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 
-
 /**
- * 
- * 
  * @author niushuai
  * @email niushuai951101@gmail.com
  * @date 2019-04-29 12:58:02
@@ -19,55 +16,46 @@ import java.util.Date;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EduWebsiteProfile implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 *
-	 * 
-	 */
-	private Integer id;
-	/**
-	 *
-	 * 类型
-	 */
-	private String type;
-	/**
-	 *
-	 * 内容JSON格式
-	 */
-	private String desciption;
-	/**
-	 *
-	 * 说明
-	 */
-	private String explain;
-	/**
-	 *
-	 * 0禁用 1启用
-	 */
-	private String state;
-	/**
-	 *
-	 * 创建人
-	 */
-	private String createBy;
-	/**
-	 *
-	 * 创建时间
-	 */
-	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date createDate;
-	/**
-	 *
-	 * 更新人
-	 */
-	private String updateBy;
-	/**
-	 *
-	 * 更新时间
-	 */
-	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-	private Date updateDate;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     */
+    private Integer id;
+    /**
+     * 类型
+     */
+    private String type;
+    /**
+     * 内容JSON格式
+     */
+    private String desciption;
+    /**
+     * 说明
+     */
+    private String explain;
+    /**
+     * 0禁用 1启用
+     */
+    private String state;
+    /**
+     * 创建人
+     */
+    private String createBy;
+    /**
+     * 创建时间
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createDate;
+    /**
+     * 更新人
+     */
+    private String updateBy;
+    /**
+     * 更新时间
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateDate;
 
 
 }
--- src/main/java/cc/niushuai/graduate/entity/SysJob.java
+++ src/main/java/cc/niushuai/graduate/entity/SysJob.java
@@ -7,10 +7,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 
-
 /**
  * Cron任务
- * 
+ *
  * @author niushuai
  * @email niushuai951101@gmail.com
  * @date 2019-05-05 10:06:03
@@ -18,68 +17,56 @@ import java.util.Date;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SysJob implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 *
-	 * id
-	 */
-	private Long id;
-	/**
-	 *
-	 * 任务名称
-	 */
-	private String jobName;
-	/**
-	 *
-	 * cron表达式
-	 */
-	private String cron;
-	/**
-	 *
-	 * 是否启动 0 未启动 1 已启动
-	 */
-	private Integer started;
-	/**
-	 *
-	 * 是否需要重复执行
-	 */
-	private Integer recycle;
-	/**
-	 *
-	 * 执行路径
-	 */
-	private String clazzPath;
-	/**
-	 *
-	 * 任务描述
-	 */
-	private String jobDesc;
-	/**
-	 *
-	 * 删除标志 0已删除 1未删除
-	 */
-	private Integer state;
-	/**
-	 *
-	 * 创建日期
-	 */
-	private Date createDate;
-	/**
-	 *
-	 * 更新日期
-	 */
-	private Date updateDate;
-	/**
-	 *
-	 * 创建人
-	 */
-	private String createBy;
-	/**
-	 *
-	 * 更新人
-	 */
-	private String updateBy;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    private Long id;
+    /**
+     * 任务名称
+     */
+    private String jobName;
+    /**
+     * cron表达式
+     */
+    private String cron;
+    /**
+     * 是否启动 0 未启动 1 已启动
+     */
+    private Integer started;
+    /**
+     * 是否需要重复执行
+     */
+    private Integer recycle;
+    /**
+     * 执行路径
+     */
+    private String clazzPath;
+    /**
+     * 任务描述
+     */
+    private String jobDesc;
+    /**
+     * 删除标志 0已删除 1未删除
+     */
+    private Integer state;
+    /**
+     * 创建日期
+     */
+    private Date createDate;
+    /**
+     * 更新日期
+     */
+    private Date updateDate;
+    /**
+     * 创建人
+     */
+    private String createBy;
+    /**
+     * 更新人
+     */
+    private String updateBy;
 
 
 }
